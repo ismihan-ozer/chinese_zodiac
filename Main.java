d0db3edc9407d8d97cd2f6fe5aca28c8e24d5b79
ismihan-ozer <98219075+ismihan-ozer@users.noreply.github.com>
2023-07-25 21:49:25 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int year,carry;
+        System.out.print("Doğum yılınızı giriniz: ");
+        year = inp.nextInt();
+        carry = year%12;
+
+        switch (carry){
+            case 0:
+                System.out.println("Çin zodyağı burcunuz: " + "Maymun");
+                break;
+            case 1:
+                System.out.println("Çin zodyağı burcunuz: " + "Horoz");
+                break;
+            case 2:
+                System.out.println("Çin zodyağı burcunuz: " + "Köpek");
+                break;
+            case 3:
+                System.out.println("Çin zodyağı burcunuz: " + "Domuz");
+                break;
+            case 4:
+                System.out.println("Çin zodyağı burcunuz: " + "Fare");
+                break;
+            case 5:
+                System.out.println("Çin zodyağı burcunuz: " + "Öküz");
+                break;
+            case 6:
+                System.out.println("Çin zodyağı burcunuz: " + "Kaplan");
+                break;
+
+            case 7:
+                System.out.println("Çin zodyağı burcunuz: " + "Tavşan");
+                break;
+            case 8:
+                System.out.println("Çin zodyağı burcunuz: " + "Ejderha");
+                break;
+            case 9:
+                System.out.println("Çin zodyağı burcunuz: " + "Yılan");
+                break;
+            case 10:
+                System.out.println("Çin zodyağı burcunuz: " + "At");
+                break;
+            case 11:
+                System.out.println("Çin zodyağı burcunuz: " + "Koyun");
+                break;
+
+        }
+
+    }
+}
